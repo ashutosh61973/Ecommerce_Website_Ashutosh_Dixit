99a117994fa21d39f0c3cb60388836bdad786582
ashutosh61973 <ashutoshd2001@gmail.com>
2022-01-20 22:43:28 +0530

    completing paging  option to apifeature

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,16 +17,20 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 //get all product
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
   // console.log(req.query);
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments();
   const apiFeature = new ApiFeatures(Product.find(), req.query)
     .search()
-    .filter();
+    .filter()
+    .pagination(resultPerPage);
 
   const products = await apiFeature.query;
 
   res.status(200).json({
     success: true,
     products,
-    size: products.length,
+    // size: products.length,
+    productCount,
   });
 });
 
