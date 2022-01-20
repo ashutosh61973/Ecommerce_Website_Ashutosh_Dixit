d499fd3645f709fc40b99d8332ed7328c7248747
ashutosh61973 <ashutoshd2001@gmail.com>
2022-01-20 12:21:36 +0530

    making api feature  and  moving forward to pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -16,9 +16,10 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 //get all product
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const apiFeature = new ApiFeatures(Product.find(), req.query);
+  // console.log(req.query);
+  const apiFeature = new ApiFeatures(Product.find(), req.query).search();
 
-  const products = await Product.find();
+  const products = await apiFeature.query;
 
   res.status(200).json({
     success: true,
